e7fd13e998407033a64fdb923031715ae16e32c0
Ao <2896068058@qq.com>
2022-01-17 20:05:15 +0800

    delete map.js

--- Map.js
+++ Map.js
@@ -1,18 +1,24 @@
-Array.prototype.maps = function (...args) {
-    if (!args.length || typeof args[0] !== 'function') throw new TypeError(`${args[0]} is not a function`)
-    if (args[1] === undefined) args[1] = globalThis
-    const arr = [],{length} = this
-    for(let i = 0; i< length; i++) arr.push(args[0].call(args[1],this[i],i,this))
-    return arr
+if (!Array.prototype.maps) {
+    Array.prototype.maps = function () {
+        var args = [];
+        for (var _i = 0; _i < arguments.length; _i++) {
+            args[_i] = arguments[_i];
+        }
+        if (!args.length || typeof args[0] !== 'function')
+            throw new TypeError("".concat(args[0], " is not a function"));
+        if (args[1] === undefined)
+            args[1] = globalThis;
+        var arr = [], length = this.length;
+        for (var i = 0; i < length; i++)
+            arr.push(args[0].call(args[1], this[i], i, this));
+        return arr;
+    };
 }
-
-const list = [1,2,3]
-
-const obj = { 
-    name:"嘿嘿嘿",
-    age:99
-}
-
+var list = [1, 2, 3];
+var obj = {
+    name: "嘿嘿嘿",
+    age: 99
+};
 // list.map()
 // list.map(1,2,3)
 // list.map(function(x,y,z) {console.log(x,y,z,this)},2,3)
@@ -20,9 +26,7 @@ const obj = {
 // list.map(function(x,y,z) {console.log(x,y,z,this)},"",3)
 // list.map(function(x,y,z) {console.log(x,y,z,this)},obj)
 // list.map((x,y,z) => console.log(x,y,z,this))
-
 // console.log("================================");
-
 // list.maps()
 // list.maps(1,2,3)
 // list.maps(function(x,y,z) {console.log(x,y,z,this)},2,3)
@@ -30,5 +34,3 @@ const obj = {
 // list.maps(function(x,y,z) {console.log(x,y,z,this)},"",3)
 // list.maps(function(x,y,z) {console.log(x,y,z,this)},obj)
 // list.maps((x,y,z) => console.log(x,y,z,this))
-
-
