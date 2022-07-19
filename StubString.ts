2e5c252fd75242c9f9078ef099288c315e6dac87
Ao <2896068058@qq.com>
2022-07-19 22:36:43 +0800

    StubString

---
+++ StubString.ts
@@ -0,0 +1,11 @@
+function stubString(frequency:number): string[] {
+    const result:string[] = [];
+    for(let i = 0; i < frequency; i++) {
+        result.push("")
+    }
+    return result
+}
+
+
+
+
