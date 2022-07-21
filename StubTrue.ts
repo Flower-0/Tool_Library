db0cb9c38f446af0217b9c43ac3f37668ae42d3e
Ao <2896068058@qq.com>
2022-07-21 17:07:19 +0800

    stubTrue

---
+++ StubTrue.ts
@@ -0,0 +1,10 @@
+export function stubTrue(params:number): boolean[] {
+    const result:boolean[] = [];
+    for (let i = 0; i <params; i++) {
+        result.push(true)
+    }
+    return result
+}
+
+
+
