cd2ef137e61cb734c8990ef6e1979de4bfda4683
Ao <2896068058@qq.com>
2022-08-22 22:09:24 +0800

    pick

---
+++ Pick.ts
@@ -0,0 +1,12 @@
+export function pick(front:any,behind:string | string[]): object {
+    const result:any = {}
+    if (typeof behind === 'string') {
+        result[behind] = front[behind]
+    } else {
+        behind.forEach(item => {
+            result[item] = front[item]
+        })
+    }   
+    return result
+}
+
